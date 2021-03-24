cdd72ddaff745ea752968d3d3c3fc1da557021b5
CaveJohnson376 <cavejohnson376@gmail.com>
2021-03-25 03:52:30 +0500

    and a comma

---
+++ arduinodata1/arduinodata1.ino
@@ -0,0 +1,127 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+unsigned long showipstarttime = 0;
+bool showip = false;
+bool showip_prev = false;
+String ip = "Disconnected\n0.0.0.0";
+
+unsigned long bpmstarttime = 0;
+unsigned long ecgstarttime = 0;
+bool bpmbusy = false;
+bool ecgbusy = false;
+bool bpmbusy_prev = false;
+bool ecgbusy_prev = false;
+
+unsigned long bpm_lastbeattime = 0;
+unsigned long bpm_lastlastbeattime = 0;
+bool bpm_state = false;
+bool bpm_state_prev = false;
+
+word ecgdata[100];
+byte ecgdatapos = 0;
+
+String wordarraytoString(word in[100]) {
+  String out = "";
+  for (byte i = 0; i < 100; i++) {
+    out += String(in[i]);
+    if (i != 99) out += ",";
+  }
+  return out;
+}
+
+void setup() {
+  Serial.begin(115200);
+  lcd.begin(16, 2);
+  lcd.print("initializing...");
+  Serial.println("starting things up");
+
+  lcd.clear();
+  showip = true;
+}
+
+void loop() {
+  String espdata = "noth";
+  if (Serial.available()) espdata = Serial.readStringUntil('\r');
+  if (espdata.startsWith("dt")) {
+    if (espdata.endsWith("startbpm")) bpmbusy = true;
+    if (espdata.endsWith("stopbpm")) bpmbusy = false;
+    if (espdata.endsWith("startecg")) ecgbusy = true;
+    if (espdata.endsWith("stopecg")) ecgbusy = false;
+  } else if (espdata.startsWith("ip"))
+    ip = espdata.substring(2);
+
+  unsigned long curtime = millis();
+  if (bpmbusy) {
+    if (!bpmbusy_prev) bpmstarttime = curtime;
+    if (curtime > bpmstarttime + 30000) bpmbusy = false;
+
+    bpm_state_prev = bpm_state;
+    bpm_state = analogRead(A0) > 512 ? true : false;
+    if (bpm_state && !bpm_state_prev) {
+      bpm_lastlastbeattime = bpm_lastbeattime;
+      bpm_lastbeattime = curtime;
+    }
+  }
+
+  if (ecgbusy) {
+    if (!ecgbusy_prev) ecgstarttime = curtime;
+    if (curtime > ecgstarttime + 30000) ecgbusy = false;
+
+    ecgdatapos = ecgdatapos == 99 ? 0 : ecgdatapos + 1;
+    ecgdata[ecgdatapos] = (digitalRead(A2) == true && digitalRead(A3) == true) ? 0 : analogRead(A1);
+  }
+
+  if (digitalRead(A4)) showip = true;
+
+  if (showip) {
+    if (!showip_prev) {
+      showipstarttime = curtime;
+      Serial.print("getIP\r");
+    }
+    if (curtime > showipstarttime + 5000) showip = false;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(ip.substring(0, ip.indexOf('\n')));
+    lcd.setCursor(0, 1);
+    lcd.print(ip.substring(ip.indexOf('\n') + 1));
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("BPM " + (bpmbusy ? "RUN" + String((byte)(bpmstarttime + 30000 - curtime)) : "STB"));
+    lcd.setCursor(0, 1);
+    lcd.print("ECG " + (bpmbusy ? "RUN" + String((byte)(bpmstarttime + 30000 - curtime)) : "STB"));
+  }
+
+  String out = "";
+  out += "{\"bpm_data\":{\"lastbeatinterval\":";
+  out += String(bpm_lastbeattime - bpm_lastlastbeattime);
+  out += ",\"time_left\":";
+  out += String((bpmstarttime + 30000 - curtime) / 1000);
+  out += ",\"is_busy\":\"";
+  out += String(bpmbusy);
+
+  out += "\"},\"ecg_data\":{\"data\":[";
+  out += wordarraytoString(ecgdata);
+  out += "],\"datapos\":";
+  out += String(ecgdatapos);
+  out += "\",\"time_left\":";
+  out += String((ecgstarttime + 30000 - curtime) / 1000);
+  out += ",\"is_busy\":\"";
+  out += String(ecgbusy);
+
+  out += "\"}\r";
+
+  char outarr[out.length()];
+  out.toCharArray(outarr, out.length());
+
+  digitalWrite(8, bpmbusy || ecgbusy);
+  digitalWrite(9, (!bpmbusy) && (!ecgbusy));
+
+  delay(20);
+
+  bpmbusy_prev = bpmbusy;
+  ecgbusy_prev = ecgbusy;
+  showip_prev = showip;
+}
---
+++ websocketesp1/websocketesp1.ino
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+WebSocketsServer webSocket = WebSocketsServer(80);
+
+String IpAddress2String(const IPAddress& ipAddress){
+  return String(ipAddress[0]) + String(".") +\
+  String(ipAddress[1]) + String(".") +\
+  String(ipAddress[2]) + String(".") +\
+  String(ipAddress[3])  ;
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  if (type == WStype_TEXT) {
+    Serial.print("dt");
+    Serial.print((char*)payload);
+    Serial.print("\r");
+  }
+
+}
+
+void setup() {
+  // USE_SERIAL.begin(921600);
+  Serial.begin(115200);
+
+  WiFiMulti.addAP("Cuba_2G4", "Cube09021922211605");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+}
+
+void loop() {
+  String ardudata = "noth";
+  if (Serial.available()) ardudata = Serial.readStringUntil('\r');
+  if (ardudata == "getIP") {
+    bool is_connected = (WiFiMulti.run() == WL_CONNECTED);
+    Serial.print(is_connected ? "ipConnected\n" : "ipDisconnected\n");
+    Serial.print(is_connected ? IpAddress2String(WiFi.localIP()) : " ");
+    Serial.print("\r");
+  } else if (ardudata == "noth") {
+    digitalWrite(3, true);
+  } else webSocket.broadcastTXT(ardudata);
+  webSocket.loop();
+}
